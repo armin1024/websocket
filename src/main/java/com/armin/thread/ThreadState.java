d3858764df8c799c73e65cb1e2f3a6a6214371d2
ARMIN <1023349234@qq.com>
2023-07-25 22:55:09 +0800

    (feat)thread: add thread test demo

---
+++ src/main/java/com/armin/thread/ThreadState.java
@@ -0,0 +1,76 @@
+package com.armin.thread;
+
+public class ThreadState {
+
+    public static void main(String[] args) throws Exception {
+        testTerminated();
+    }
+
+    public static void testNewAndRunnable() {
+        Thread thread = new Thread(() -> {});
+        System.out.println(thread.getState()); //NEW
+        thread.start();
+        System.out.println(thread.getState()); //RUNNABLE
+    }
+
+    public static void testBlocked() throws InterruptedException {
+        Object obj = new Object();
+        Thread thread = new Thread(() -> {
+            //thread未获取到obj锁资源，所以状态为阻塞
+            synchronized (obj) {
+                System.out.println("execute");
+            }
+        });
+        //主线程获取到了锁资源
+        synchronized (obj) {
+            thread.start();
+            Thread.sleep(1);
+            System.out.println(thread.getState()); //BLOCKED
+        }
+    }
+
+    public static void testWaiting() throws Exception {
+        Object obj = new Object();
+        Thread thread = new Thread(() -> {
+            synchronized (obj) {
+                try {
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+        Thread.sleep(500);
+        System.out.println(thread.getState()); //WAITING
+    }
+
+    public static void testTimedWaiting() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        //thread线程启动后，主线程休眠了0.5秒再获取thread的状态，此时thread线程还在休眠中
+        thread.start();
+        Thread.sleep(500);
+        System.out.println(thread.getState()); //TIMED_WAITING
+    }
+
+    public static void testTerminated() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        //thread线程启动后，主线程休眠了1秒再获取thread的状态，此时thread线程已经执行完毕
+        thread.start();
+        Thread.sleep(1000);
+        System.out.println(thread.getState()); //TERMINATED
+    }
+
+}
---
+++ src/main/java/com/armin/thread/ThreadTest.java
@@ -0,0 +1,48 @@
+package com.armin.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class ThreadTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        /*for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }*/
+        XssCallable xssCallable = new XssCallable(128F, 120000F);
+        FutureTask<Float> task = new FutureTask<>(xssCallable);
+        Thread thread = new Thread(task);
+        thread.start();
+        //do other things
+        System.out.println("做的其他计算工作...");
+        System.out.println("阻塞获取异步线程数据...");
+        System.out.println(task.get());
+    }
+
+}
+
+class XssCallable implements Callable<Float> {
+
+    private Float x;
+    private Float y;
+
+    public XssCallable(Float x, Float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public Float call() throws Exception {
+        System.out.println("开始睡觉...");
+        Thread.sleep(5000);
+        System.out.println("睡醒了！");
+        return x / y * 100;
+    }
+}

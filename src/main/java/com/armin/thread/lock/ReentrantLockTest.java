1de795547daabbf8aa522c522f9bb1952d5affa2
ARMIN <1023349234@qq.com>
2023-08-10 22:45:30 +0800

    (feat)thread: add ReentrantLockTest

---
+++ src/main/java/com/armin/thread/lock/ReentrantLockTest.java
@@ -0,0 +1,20 @@
+package com.armin.thread.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+
+    public static void main(String[] args) {
+        //默认是非公平锁
+        ReentrantLock lock1 = new ReentrantLock(); //无参构造默认非公平锁
+        //有参构造设置为true：公平锁
+        ReentrantLock lock2 = new ReentrantLock(true);
+        lock1.lock();
+        try {
+            System.out.println("业务代码");
+        } finally {
+            lock1.unlock();
+        }
+    }
+
+}

80cc48d752d6f592003e05d5020efd2ac1d9cf79
armin1024 <kandaiming77>
2023-08-27 09:34:25 +0800

    feat(thread): add PriorityBlockingQueueTest

---
+++ src/main/java/com/armin/thread/blockingqueue/PriorityBlockingQueueTest.java
@@ -0,0 +1,26 @@
+package com.armin.thread.blockingqueue;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class PriorityBlockingQueueTest {
+
+    public static void main(String[] args) {
+        PriorityBlockingQueue<Integer> intQueue = new PriorityBlockingQueue<>();
+        PriorityBlockingQueue<String> strQueue = new PriorityBlockingQueue<>();
+        intQueue.add(1);
+        intQueue.add(10);
+        intQueue.add(5);
+        intQueue.add(3);
+        strQueue.add("456");
+        strQueue.add("123");
+        strQueue.add("345");
+        System.out.println(intQueue.poll());
+        System.out.println(intQueue.poll());
+        System.out.println(intQueue.poll());
+        System.out.println("=============");
+        System.out.println(strQueue.poll());
+        System.out.println(strQueue.poll());
+        System.out.println(strQueue.poll());
+    }
+
+}

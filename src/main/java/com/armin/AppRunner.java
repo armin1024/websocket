a6a1b3b57ea4b58be433c236ed1152209c6bfa08
armin1024 <kandaiming77>
2023-09-19 10:07:50 +0800

    feat(thread): add notice for CountDownLatchTest

--- src/main/java/com/armin/AppRunner.java
+++ src/main/java/com/armin/AppRunner.java
@@ -10,39 +10,39 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AppRunner {
 
     public static void main(String[] args) throws InterruptedException {
-//        SpringApplication.run(AppRunner.class, args);
-        ReentrantLock lock = new ReentrantLock();
-        Condition condition = lock.newCondition();
-        Thread t1 = new Thread(() -> {
-            lock.lock();
-            try {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println(Thread.currentThread().getName() + ":" + i);
-                    Thread.sleep(1000);
-                    System.out.println("t1 wait");
-                    condition.await();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            lock.lock();
-            try {
-                Thread.sleep(1000);
-                System.out.println("t1 notify");
-                condition.signal();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        });
-        t1.start();
-        t2.start();
-        Thread.sleep(100);
+        SpringApplication.run(AppRunner.class, args);
+//        ReentrantLock lock = new ReentrantLock();
+//        Condition condition = lock.newCondition();
+//        Thread t1 = new Thread(() -> {
+//            lock.lock();
+//            try {
+//                for (int i = 0; i < 5; i++) {
+//                    System.out.println(Thread.currentThread().getName() + ":" + i);
+//                    Thread.sleep(1000);
+//                    System.out.println("t1 wait");
+//                    condition.await();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } finally {
+//                lock.unlock();
+//            }
+//        });
+//        Thread t2 = new Thread(() -> {
+//            lock.lock();
+//            try {
+//                Thread.sleep(1000);
+//                System.out.println("t1 notify");
+//                condition.signal();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } finally {
+//                lock.unlock();
+//            }
+//        });
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(100);
     }
 
 }
